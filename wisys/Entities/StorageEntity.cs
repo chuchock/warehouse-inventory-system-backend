2c54acba480f51090a8a18a752bbe751f4902768
Jesús Montero <jesusmontero.developer@gmail.com>
2020-07-29 13:53:47 -0500

    Added Storage entity

---
+++ wisys/Entities/StorageEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wisys.Entities
+{
+	public class StorageEntity
+	{
+		public string StorageId { get; set; }
+		public DateTime LastUpdate { get; set; }
+		public int PartialQuantity { get; set; }
+		public string ProductId { get; set; }
+		public ProductEntity Product { get; set; }
+		public string WarehouseId { get; set; }
+		public WarehouseEntity Warehouse { get; set; }
+	}
+}
