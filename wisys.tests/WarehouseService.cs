387c9c929a6d8651fd6ea20c48cfe53d72268875
Jesús Montero <jesusmontero.developer@gmail.com>
2020-07-29 13:03:20 -0500

    Weather controller removed

---
+++ wisys.tests/WarehouseService.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace wisys.tests
+{
+	public class WarehouseService
+	{
+		private readonly WarehouseController _warehouseController;
+
+		[Fact]
+		public void GetTest_ReturnsListOfWarehouses()
+		{
+			//arrange
+			var mockPosts = new List<Post> {
+				new Post{Title = "Tdd One"},
+				new Post{Title = "Tdd and Bdd"}
+			};
+
+			_mockPostsList.Object.AddRange(mockPosts);
+
+			//act
+			var result = WarehouseController.Get();
+
+			//assert
+			var model = Assert.IsAssignableFrom<ActionResult<List<Post>>>(result);
+			Assert.Equal(2, model.Value.Count);
+		}
+	}
+}
---
+++ wisys/Models/Warehouse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wisys.Models
+{
+	public class Warehouse
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public string Address { get; set; }
+	}
+}
