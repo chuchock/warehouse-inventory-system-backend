1e42122b73b1ae49810a1e25ea143085d65fe3c8
Jesús Montero <jesusmontero.developer@gmail.com>
2020-07-29 13:44:04 -0500

    Added Category, Product, Warehouse entities

---
+++ wisys/Entities/CategoryEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+
+namespace wisys.Entities
+{
+	public class CategoryEntity
+	{
+		public int CategoryId { get; set; }
+		public string Name {get; set;}
+		public ICollection<ProductEntity> Products { get; set; }
+	}
+}
---
+++ wisys/Entities/ProductEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wisys.Entities
+{
+	public class ProductEntity
+	{
+		public int ProductId { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public int Quantity { get; set; }
+		public decimal BuyPrice { get; set; }
+		public decimal SalePrice { get; set; }
+		public DateTime Date { get; set; }
+		public string CategoryId { get; set; }
+		public CategoryEntity Category { get; set; }
+	}
+}
