09738864cdfda01e5c11939ef02dff4a68b0afb3
Jesús Montero <jesusmontero.developer@gmail.com>
2020-07-29 14:32:34 -0500

    Added IWarehouseRepository

---
+++ wisys/Services/IWarehouseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wisys.Entities;
+
+namespace wisys.Services
+{
+	interface IWarehouseRepository
+	{
+		List<WarehouseEntity> GetAllWarehousesAsync();
+		WarehouseEntity GetWarehouseByIdAsync(int Id);
+		void AddWarehouseAsync(WarehouseEntity warehouse);
+		void UpdateWarehouseAsync(WarehouseEntity warehouse);
+		void DeleteWarehouseAsync(int Id);
+	}
+}
