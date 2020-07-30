1f8347995e7474724310a63551970c87988b269b
Jesús Montero <jesusmontero.developer@gmail.com>
2020-07-30 12:35:26 -0500

    Added pagination general helper

--- wisys/Controllers/ProductsController.cs
+++ wisys/Controllers/ProductsController.cs
@@ -1,9 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using wisys.Data;
+using wisys.DTOs;
 using wisys.Entities;
+using wisys.Helpers;
 using wisys.Services;
 
 namespace wisys.Controllers
@@ -11,21 +13,31 @@ namespace wisys.Controllers
 	[Route("api/products")]
 	public class ProductsController : ControllerBase
 	{
+		private readonly AppDbContext dbContext;
 		private readonly IProductRepository repository;
 
-		public ProductsController(IProductRepository repository)
+
+		public ProductsController(AppDbContext dbContext, IProductRepository repository)
 		{
 			this.repository = repository;
+			this.dbContext = dbContext;
 		}
+
 
 		// api/products
 		[HttpGet]
-		public async Task<ActionResult<List<ProductEntity>>> Get()
+		public async Task<ActionResult<List<ProductEntity>>> Get([FromQuery] PaginationDTO pagination)
 		{
-			var products = await repository.GetAllProductsAsync();
+			var queryable = dbContext.Products.AsQueryable();
+			await HttpContext.InsertPaginationParametersInResponse(queryable, pagination.RecordsPerPage);
+
+			var products = await queryable.Paginate(pagination).ToListAsync();
+
+			//var products = await repository.GetAllProductsAsync();
 
 			return products;
 		}
+
 
 		// api/products/{Id}
 		[HttpGet("{id}")]
@@ -41,6 +53,7 @@ namespace wisys.Controllers
 			return product;
 		}
 
+
 		// api/products
 		[HttpPost]
 		public async Task<ActionResult> Post(ProductEntity product)
@@ -49,6 +62,7 @@ namespace wisys.Controllers
 
 			return NoContent();
 		}
+
 
 		// api/products
 		[HttpPut("{id}")]
@@ -63,6 +77,7 @@ namespace wisys.Controllers
 
 			return NoContent();
 		}
+
 
 		// api/products/{Id}
 		[HttpDelete("{id}")]
---
+++ wisys/Helpers/QueryableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wisys.DTOs;
+
+namespace wisys.Helpers
+{
+	public static class QueryableExtensions
+	{
+		public static IQueryable<T> Paginate<T>(this IQueryable<T> queryable, PaginationDTO pagination)
+		{
+			return queryable
+				.Skip((pagination.Page - 1) * pagination.RecordsPerPage)
+				.Take(pagination.RecordsPerPage);
+		}
+	}
+}
